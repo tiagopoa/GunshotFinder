2ba99cfd15e4f8c1013a28e2ee4267f573914801
Tiago Correa <tiagopoa@gmail.com>
2018-06-20 18:21:37 -0300

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
+  styles: [`
+    agm-map {
+      height: 300px;
+    }
+  `],
+  template: `
+  <agm-map [latitude]="lat" [longitude]="lng"></agm-map>
+  `,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app works!';
+  zoom: number = 8;
+  lat: number = 51.678418;
+  lng: number = 7.809007;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,22 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+//import { HelloComponent } from './hello.component';
+import { AgmCoreModule } from '@agm/core';
+
+
 import { AppComponent } from './app.component';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
+@NgModule({  
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAifS9CFk1pbp5YIbv2NAQexZC9K55yarU'
+    })
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
